0a1399f6e3b19fcef67468977b8b2dbb0d397e55
Sourav R <developer.srv1@gmail.com>
2021-07-10 19:08:39 +0530

    test component

---
+++ app/View/Components/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Devsrv\Aquastrap\Traits\ExposeMethods;
+
+class Post extends Component
+{
+    use ExposeMethods;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.post');
+    }
+}
