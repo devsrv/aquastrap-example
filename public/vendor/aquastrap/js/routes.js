f96ba633d2dd03715ce75c95d8b74469bfe34e96
Sourav R <developer.srv1@gmail.com>
2021-07-14 19:38:24 +0530

    asset publish

---
+++ public/vendor/aquastrap/js/routes.js
@@ -0,0 +1,33 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/routes.js ***!
+  \********************************/
+window._aquastrap = window._aquastrap || {
+  component: [{
+    id: 'article',
+    routes: {
+      "update": "http://127.0.0.1:8000/articles/foo/update",
+      "delete": "http://127.0.0.1:8000/articles/foo/delete"
+    },
+    config: {}
+  }, {
+    id: 'post',
+    routes: {
+      "delete": "http://127.0.0.1:8000/aquastrap/145b16fffaf48b09d39c7339d77c3ae6/delete"
+    },
+    config: {}
+  }, {
+    id: 'folder.my-component',
+    routes: {
+      "delete": "http://127.0.0.1:8000/aquastrap/d5796c8780c470a876faf663ba4bf5da/delete"
+    },
+    config: {}
+  }],
+  config: {
+    success: function success() {},
+    error: function error() {}
+  }
+};
+/******/ })()
+;
