91d380215b6eac8f878ff5245bce9b241131b172
Sourav R <developer.srv1@gmail.com>
2021-08-14 19:17:05 +0530

    vue examples added

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -5,10 +5,10 @@
     <div class="container">
         <div class="row">
             <div class="col-4">
-                <x-article />
+                {{-- <x-article /> --}}
             </div>
             <div class="col-4">
-                <x-folder.my-component fname="rv" username="rav" :aCallableArg="function($param) { return $param; }" />
+                {{-- <x-folder.my-component fname="rv" username="rav" :aCallableArg="function($param) { return $param; }" /> --}}
 
                 <x-wrapper wrap="div.row>div.col-6>div.card.p-2>div.card-body>div.form-group[title='Hello world']>span.bg-info+p#rav>p">
                     <x-form.text class="form-control-sm" />
@@ -17,6 +17,8 @@
             <div class="col-4">
                 <x-post username="rsourav" comment="test comment" foo="bar" />
                 <x-post username="coldsierra" comment="shiva" />
+                <hr>
+                <x-vue-form />
             </div>
         </div>
     </div>
