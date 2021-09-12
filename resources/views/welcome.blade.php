b1d760a10619432c5bf1c79a5196c75b532a97b1
Sourav R <developer.srv1@gmail.com>
2021-09-12 23:29:25 +0530

    listen window aqua notification evt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,6 +34,12 @@
     <body class="container-fluid bg-secondary">
         @yield('content')
 
+        <script>
+            window.addEventListener('aqua.notification', function(e) {
+                console.log(e.detail);
+            });
+        </script>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
